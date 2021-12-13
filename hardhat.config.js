c4ec8f55999867c78223bf4a1aca3ed5b4fc0e4b
Hazel <xp0729@gmail.com>
2021-12-13 21:32:09 +0800

    update contract to erc20

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,7 @@ require("@nomiclabs/hardhat-ethers");
 const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
-   solidity: "0.8.10",
+   solidity: "0.8.0",
    networks: {
       rinkeby: {
          url: API_URL,
