1466bc239b5dad0f4646c68de4e58b77185b8f89
Hazel <xp0729@gmail.com>
2021-12-13 15:06:57 +0800

    init

---
+++ scripts/mint.js
@@ -0,0 +1,12 @@
+const hre = require("hardhat");
+
+async function main() {
+  const NFT = await hre.ethers.getContractFactory("FeedMob");
+  const { CONTRACT_ADDRESS } = process.env;
+  const contract = NFT.attach(CONTRACT_ADDRESS);
+  await contract.mint();
+}
+main().then(() => process.exit(0)).catch(error => {
+  console.error(error);
+  process.exit(1);
+});
