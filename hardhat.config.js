5c47679f1b9f602fe7d591800676d63f6f2150b7
Ian <xiongbo027@gmail.com>
2021-12-28 10:11:50 +0000

    add etherscan to config

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,13 +4,18 @@
 
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan"); 
 
-const { API_URL, PRIVATE_KEY } = process.env;
+const { API_KEY, API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
    solidity: "0.8.0",
+   etherscan: {
+      apiKey: API_KEY,
+   },
    networks: {
-      rinkeby: {
+      main: {
+	 chainId: 43114,
          url: API_URL,
          accounts: [PRIVATE_KEY]
       }
